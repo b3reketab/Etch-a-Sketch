9c9afd1a7790d374b2eb4755319841cd1d1640fa
b3reketab <141371508+b3reketab@users.noreply.github.com>
2023-10-14 14:47:23 +0300

    add different colors

--- script.js
+++ script.js
@@ -1,38 +1,31 @@
 const container = document.querySelector('div');
 const btn = document.querySelector('#one');
 let side = 16;
-let x = (400 / side) - 2;
-let homePage = function () {
-    for (let i = 0; i < side * side; i++) {
-        const divb = document.createElement('div');
-        divb.setAttribute('class', 'divb');
-        divb.addEventListener('mouseover', () => {
-            divb.style.backgroundColor = 'black';
-        })
-        divb.style.width = `${x}px`;
-        divb.style.height = `${x}px`;
-        container.appendChild(divb);
-    }
-};
-homePage();
 function getSqr () {
-    const input = prompt('How many squares should there be on the side of the grid?', );
-    side = input;
-    x = (400 / side) - 2;
+    let x = (400 / side) - 2;
     for (let i = 0; i < side * side; i++) {
         const divb = document.createElement('div');
         divb.setAttribute('class', 'divb');
         divb.addEventListener('mouseover', () => {
-            divb.style.backgroundColor = 'black';
+            let color = () => {
+                if (Math.random() < 0.25) return 'green';
+                else if (Math.random() < 0.5) return 'yellow';
+                else if (Math.random() < 0.75) return 'red';
+                else return 'blue';
+            }
+            divb.style.backgroundColor = color();
         })
         divb.style.width = `${x}px`;
         divb.style.height = `${x}px`;
         container.appendChild(divb);
     }
 }
+getSqr();
 btn.addEventListener('click', (event) => {
     while (container.firstChild) {
         container.removeChild(container.lastChild);
       }
+    const input = prompt('How many squares should there be on the side of the grid?', );
+    side = input;
     getSqr();
 });
